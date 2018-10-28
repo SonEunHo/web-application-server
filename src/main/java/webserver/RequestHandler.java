6f5340898c49a80d2e680891962d39d7a133b519
nano.son <nano.son@kakaocorp.com>
2018-10-29 00:06:26 +0900

    index.html 읽어서 내보내기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,17 +1,23 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import com.google.common.base.Strings;
+
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
-
+    private static final String PROJECT_ROOT = "/Users/kakao/workspace/web-application-server/";
     private Socket connection;
 
     public RequestHandler(Socket connectionSocket) {
@@ -23,14 +29,60 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+            String requestDocument = readRequest(new BufferedReader(new InputStreamReader(in)));
+
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World!!".getBytes();
+            byte[] body;
+
+            log.debug("requestDocument: {}", requestDocument);
+            if(requestDocument != null) {
+                File f = new File(PROJECT_ROOT+requestDocument);
+
+                String line = "";
+                String temp;
+                try(BufferedReader br = new BufferedReader(new FileReader(f))){
+                    while((temp = br.readLine()) != null)
+                        line += temp;
+                } catch (Exception e) {
+                    log.error(e.getMessage());
+                }
+                body = line.getBytes();
+            } else {
+                body = "Hello World!!".getBytes();
+            }
             response200Header(dos, body.length);
             responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
+    }
+
+    // RequestReader와 같은 전담 클래스를 만드는게 어떨까?
+    private String readRequest(BufferedReader br) {
+        String line = null;
+        String requestDocument = null;
+        log.debug("\n\n-----------[read Request]");
+        boolean firstLine = true;
+        try {
+            while ((line = br.readLine()) != null) {
+                log.debug(line);
+                if(firstLine) {
+                    if(line.contains("GET")) {
+                        requestDocument = line.split(" ")[1];
+                        if(requestDocument.equals("/")) requestDocument = "/index.html";
+                        requestDocument = "webapp"+requestDocument;
+                    }
+                    firstLine = false;
+                } else {
+                    if(Strings.isNullOrEmpty(line)) break;
+                }
+            }
+        } catch (Exception e) {
+            log.error("readRequest Error: {}", e.getMessage());
+        }
+
+        return requestDocument;
     }
 
     private void response200Header(DataOutputStream dos, int lengthOfBodyContent) {
